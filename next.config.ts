fae2d83b22ddf3192c86d827a6856cbc31208e5d
Ghaziriyadh <ghazriyadh2@gmail.com>
2026-02-13 00:55:08 +0300

    feat: Containerize the Next.js application with Docker and Docker Compose, integrating PostgreSQL and enabling standalone build output.

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const withNextIntl = createNextIntlPlugin(
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
 };
 
 export default withNextIntl(nextConfig);
